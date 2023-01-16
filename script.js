2e60eb9666e7de555fd2e3b1552f38f46906e52e
Insane Dev <89750356+PlasmicRays@users.noreply.github.com>
2023-01-16 11:32:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var mouseEvent = "empty";
+var last_position_of_x, last_position_of_y;
+
+var width = screen.width;
+
+new_width = screen.width - 70;
+new_height = screen.height - 300;
+
+if (width < 992) {
+    document.getElementById("myCanvas").width = new_width;
+    document.getElementById("myCanvas").height = new_height;
+    document.body.style.overflow = "hidden";
+}
+
+
+
+
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+color = "black"
+width_of_line = 1;
+
+canvas.addEventListener("touchstart", my_touchstart);
+
+function my_touchstart(e) {
+    console.log("my_touchstart");
+    color = document.getElementById("color").value;
+    width_of_line = document.getElementById("width_of_line").value;
+
+    last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
+    last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
+}
+
+canvas.addEventListener("touchmove", my_touchmove);
+
+function my_touchmove(e) {
+    console.log("my_touchmove")
+    current_position_of_touch_x = e.touches[0].clientX - canvas.offsetLeft;
+    current_position_of_touch_y = e.touches[0].clientY - canvas.offsetTop;
+
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width_of_line;
+
+    console.log("Last position of X and Y coordinates = ");
+    console.log("x =" + last_position_of_x + "y = " + last_position_of_y);
+    ctx.moveTo(last_position_of_x, last_position_of_y);
+
+    console.log("Current position of X and Y coordinates = ");
+    console.log("x =" + current_position_of_touch_x + "y = " + current_position_of_touch_y);
+    ctx.lineTo(current_position_of_touch_x, current_position_of_touch_y);
+    ctx.stroke();
+
+    last_position_of_x = current_position_of_touch_x;
+    last_position_of_y = current_position_of_touch_y;
+
+}
+
+function clearArea() {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
